313086798d213a366529c3617c85de4a84a026cc
nirmal251992 <34858148+nirmal251992@users.noreply.github.com>
2017-12-27 14:35:51 +0530

    Add files via upload

---
+++ Smiley/Smiley/AddTaskVC.swift
@@ -0,0 +1,60 @@
+//
+//  AddTaskVC.swift
+//  Smiley
+//
+//  Created by Nirmal  on 25/12/17.
+//  Copyright © 2017 Nirmal . All rights reserved.
+//
+
+import UIKit
+
+class AddTaskVC: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var needSmiley: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        
+        let guest = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = guest.persistentContainer.viewContext
+        
+        let task = Task(context: context)
+        
+        if (textField.text?.isEmpty)!{
+            let alert = UIAlertController(title: "Please Add task", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        else {
+            
+            if let name = textField.text{
+                
+                task.name = name
+            }
+            
+            
+        }
+        
+         task.smiley = needSmiley.isOn
+        
+        guest.saveContext()
+        
+        navigationController?.popViewController(animated: true)
+        
+        
+    }
+    
+    
+}
---
+++ Smiley/Smiley/MainVC.swift
@@ -0,0 +1,96 @@
+//
+//  MainVC.swift
+//  Smiley
+//
+//  Created by Nirmal  on 25/12/17.
+//  Copyright © 2017 Nirmal . All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+      tableView.delegate = self
+      tableView.dataSource = self
+        
+        grabData()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        grabData()
+        
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = UITableViewCell()
+        
+        
+        let task = tasks[indexPath.row]
+        
+        if task.name != nil{
+            
+            cell.textLabel?.text = "😀\(String(describing: task.name!))☺️"
+
+        }
+      
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+      
+        let guest = UIApplication.shared.delegate as! AppDelegate
+        let context = guest.persistentContainer.viewContext
+        
+        if editingStyle == .delete{
+            
+            let task = tasks[indexPath.row]
+            
+            context.delete(task)
+            guest.saveContext()
+            grabData()
+            tableView.reloadData()
+            
+        }
+        
+    }
+    
+    func grabData() {
+        
+        let guest = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = guest.persistentContainer.viewContext
+        
+        
+        do
+        {
+            tasks = try context.fetch(Task.fetchRequest())
+        } catch{
+            print("Error")
+            
+            
+            
+        }
+    }
+    
+}
+    
+    
+    
+    
+    
+
+
